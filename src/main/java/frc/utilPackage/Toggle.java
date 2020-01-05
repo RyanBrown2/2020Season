b327a2e3597caf46887728cd424b8770f3fb570f
Ryan Brown <ryanbrown4610@gmail.com>
2020-01-04 17:59:55 -0800

    End of day 1

---
+++ src/main/java/frc/utilPackage/Toggle.java
@@ -0,0 +1,22 @@
+package frc.utilPackage;
+
+public class Toggle {
+	boolean toggle = false, pInput = false;
+	
+	public Toggle(boolean init) {
+		toggle = init;
+	}
+	
+	public boolean toggleVar(boolean input){
+		if(input && !pInput){
+			toggle = !toggle;
+		}
+		pInput = input;
+		return toggle;
+	}
+	
+	public boolean getVal(){
+		return toggle;
+	}
+	
+}
