7b553cf32dab82cb0812d94b0480dd612fb73e81
Ryan Brown <ryanbrown4610@gmail.com>
2020-02-28 22:49:51 -0800

    Adding vars

--- src/main/java/frc/util/Vision.java
+++ src/main/java/frc/util/Vision.java
@@ -35,49 +35,23 @@ public class Vision {
         }
     }
 
-    public void loadData() {
-        map.put((double) 7, new double[]{3900, 125});
-        map.put(7.3, new double[]{3900, 125});
-        map.put(7.6, new double[]{3900, 125});
-        map.put((double) 8, new double[]{3900, 125});
-        map.put(8.3, new double[]{3900, 125});
-        map.put(8.6, new double[]{3900, 125});
-        map.put((double) 9, new double[]{3900, 120});
-        map.put(9.3, new double[]{3900, 108});
-        map.put(9.6, new double[]{3900, 98});
-        map.put((double) 10, new double[]{3900, 90});
-        map.put(10.3, new double[]{3900, 85});
-        map.put(10.6, new double[]{3900, 80});
-        map.put((double) 11, new double[]{3900, 80});
-        map.put(11.3, new double[]{3900, 80});
-        map.put(11.6, new double[]{3900, 68});
-        map.put((double) 12, new double[]{4500, 30});
-        map.put(12.3, new double[]{4500, 30});
-        map.put(12.6, new double[]{4500, 30});
-        map.put((double) 13, new double[]{4500, 30});
-        map.put(13.3, new double[]{4500, 30});
-        map.put(13.6, new double[]{4500, 30});
-        map.put((double) 14, new double[]{4500, 40});
-        map.put(14.3, new double[]{4500, 30});
-        map.put(14.6, new double[]{4500, 30});
-        map.put((double) 15, new double[]{4500, 30});
-        map.put(15.3, new double[]{4000, 30});
-        map.put(15.6, new double[]{4000, 30});
-        map.put((double) 16, new double[]{4000, 30});
-        map.put(16.5, new double[]{4000, 30});
-        map.put((double) 17, new double[]{3800, 30});
-        map.put(17.5, new double[]{3800, 30});
-        map.put((double) 18, new double[]{3800, 30});
-        map.put(18.5, new double[]{3800, 30});
-        map.put((double) 19, new double[]{3900, 30});
-        map.put(19.5, new double[]{4000, 30});
-        map.put((double) 20, new double[]{4000, 30});
-        map.put(20.5, new double[]{4000, 33});
-        map.put((double) 21, new double[]{4100, 33});
+    public double[][] data = {
+            new double[]{7, 3900, 125},
+            new double[]{7.3, 3900, 125},
+            new double[]{7.6, 3900, 125},
+            new double[]{8, 3900, 125},
+            new double[]{8.3, 3900, 125},
+            new double[]{8.6, 3900, 125},
+            new double[]{9, 3900, 120},
+            new double[]{9.3, 3900, 108},
+            new double[]{9.6, 3900, 98},
+            new double[]{10, 3900, 90},
+            new double[]{10.3, 3900, 85},
+            new double[]{}
+    };
 
+    public double[] dataLookUp(double distance) {
     }
-
-
 
     /*
     Get data from Coprocessor
