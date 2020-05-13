dee66c16556e7de4c4c55b4ca6dae1fb30286e7e
unknown <ryanbrown4610@gmail.com>
2020-05-12 22:58:38 -0700

    Adding linearTransform class

---
+++ src/main/java/frc/utilPackage/linearTransform.java
@@ -0,0 +1,20 @@
+package frc.utilPackage;
+
+public class linearTransform {
+
+    // Translate a point w/ linear algebra
+    public static double[] translate(double[] inputMatrix, double[][] translationMatrix) {
+        return new double[]{
+                inputMatrix[0]*translationMatrix[0][0] + inputMatrix[1]*translationMatrix[1][0],
+                inputMatrix[0]*translationMatrix[0][1] + inputMatrix[1]*translationMatrix[1][1]
+        };
+    }
+
+    // Create a translation matrix from and angle
+    public static double[][] angleToMatrix(double angle) {
+        return new double[][]{
+                new double[]{Math.cos(angle), Math.sin(angle)},
+                new double[]{-Math.sin(angle), Math.cos(angle)}
+        };
+    }
+}
