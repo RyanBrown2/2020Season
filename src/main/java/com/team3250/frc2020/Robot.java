e7a60a0cde45e191835e8b93b84ab40270769d7b
Ryan Brown <ryanbrown4610@gmail.com>
2020-05-15 19:54:08 -0700

    Code Build Successfull, Ready To Test

--- src/main/java/com/team3250/frc2020/Robot.java
+++ src/main/java/com/team3250/frc2020/Robot.java
@@ -1,12 +1,64 @@
 package com.team3250.frc2020;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import com.team254.lib.geometry.Pose2d;
+import com.team254.lib.geometry.Rotation2d;
+import com.team254.lib.util.CrashTracker;
+import com.team254.lib.wpilib.TimedRobot;
+import com.team3250.frc2020.controlBoard.ControlBoard;
+import com.team3250.frc2020.controlBoard.IControlBoard;
+import com.team3250.frc2020.loops.Loop;
+import com.team3250.frc2020.loops.Looper;
+import com.team3250.frc2020.subsystems.Drive;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends TimedRobot {
+  private final Looper mEnabledLooper = new Looper();
+  private final Looper mDisabledLooper = new Looper();
+
+  private final SubsystemManager mSubsystemManager = SubsystemManager.getInstance();
+
+  // Subsystems
+  private final RobotState mRobotState = RobotState.getInstance();
+
+  private final Drive mDrive = Drive.getInstance();
+
+  private boolean mHasBeenEnabled = false;
+  private double mOffsetOverride = -1.0;
+
+  Robot() {
+    CrashTracker.logRobotConstruction();
+  }
 
   @Override
   public void robotInit() {
+    try {
+      CrashTracker.logRobotInit();
 
+      mSubsystemManager.setSubsystems(
+              mDrive
+      );
+
+      mRobotState.reset(Timer.getFPGATimestamp(), Pose2d.identity(), Rotation2d.identity());
+      mDrive.setHeading(Rotation2d.identity());
+
+    } catch (Throwable t) {
+      CrashTracker.logThrowableCrash(t);
+      throw t;
+    }
+  }
+
+  @Override
+  public void disabledInit() {
+    try {
+      CrashTracker.logDisabledInit();
+      mEnabledLooper.stop();
+      mDisabledLooper.start();
+
+      mDrive.setBrakeMode(false);
+    } catch (Throwable t) {
+      CrashTracker.logThrowableCrash(t);
+      throw t;
+    }
   }
 
   @Override
@@ -36,11 +88,6 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testPeriodic() {
-
-  }
-
-  @Override
-  public void disabledInit() {
 
   }
 
