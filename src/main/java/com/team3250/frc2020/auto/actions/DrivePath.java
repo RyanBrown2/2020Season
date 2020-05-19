7080b9b52ea5cf2af3092a8c78b05050f23dfb5f
unknown <ryanbrown4610@gmail.com>
2020-05-18 17:36:09 -0700

    Added Some Actions, Code Still Needs To Be Tested

---
+++ src/main/java/com/team3250/frc2020/auto/actions/DrivePath.java
@@ -0,0 +1,16 @@
+package com.team3250.frc2020.auto.actions;
+
+import com.team254.lib.control.Path;
+import com.team254.lib.util.DriveSignal;
+
+/**
+ * Drives the robot along the Path defined in the PathContainer object. The action finishes once the robot reaches the
+ * end of the path.
+ *
+ * @see PathContainer
+ * @see Path
+ * @see Action
+ */
+public class DrivePath {
+
+}
